7a50e16e547a399cde0941c0d198736ee8e94d47
Palasak <pe123450@gmail.com>
2022-02-20 00:33:19 +0700

    Component as png

--- pixels/src/components/Pixel.js
+++ pixels/src/components/Pixel.js
@@ -8,5 +8,31 @@ export default function Pixel(props) {
     const [oldColor, setOldColor] = useState(pixelColor);
     const [canChangeColor, setCanChangeColor] = useState(true);
 
-    return <div className="pixel" style={{backgroundColor: pixelColor }} ></div>;
+    function applyColor() {
+        setPixelColor(selectedColor);
+        setCanChangeColor(false);
+    }
+
+    function changeColorOnHover() {
+        setOldColor(pixelColor);
+        setPixelColor(selectedColor);
+    }
+
+    function resetColor() {
+        if (canChangeColor) {
+            setPixelColor(oldColor);
+        }
+
+        setCanChangeColor(true);
+    }
+
+    return (
+        <div 
+            className="pixel" 
+            onClick={applyColor} 
+            onMouseEnter={changeColorOnHover} 
+            onMouseLeave={resetColor} 
+            style={{backgroundColor: pixelColor }} 
+        ></div>
+    );
 }
