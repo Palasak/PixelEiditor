ac819fc7ab3631217b960468dfce1065e9a82cee
Palasak <pe123450@gmail.com>
2022-02-22 03:27:27 +0700

    Change Color Picker

--- pixels/src/components/Editor.js
+++ pixels/src/components/Editor.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../styles/editor.scss";
-import { CirclePicker } from "react-color";
+import { SwatchesPicker } from "react-color";
 import DrawingPanel from "./DrawingPanel";
 
 export default function Editor() {
@@ -58,7 +58,7 @@ export default function Editor() {
             </button>
 
             {hideOptions && (
-                <CirclePicker color={selectedColor} onChangeComplete={changeColor} />
+                <SwatchesPicker color={selectedColor} onChangeComplete={changeColor} />
             )}
 
             {hideOptions && (
